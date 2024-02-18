3779c3a96803dc70531cdb711bc2c1abbc411ace
ggalindo-2022147 <ggalindo-2022147@kinal.edu.gt>
2024-02-18 14:18:54 -0600

    refactorizacion

--- code/models/server.js
+++ code/models/server.js
@@ -1,38 +1,37 @@
-const express = require('express');
-const cors = require('cors');
-const { dbConnection } = require('../db/config');
+const express = require("express");
+const cors = require("cors");
+const { dbConnection } = require("../db/config");
 
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.usuariosPath = "/api/usuarios";
 
-class Server{
-    constructor(){
-        this.app = express();
-        this.port = process.env.PORT; 
-        this.usuariosPath = '/api/usuarios';
+    this.conectarDB();
+    this.middlewares();
+    this.routes();
+  }
 
-        this.conectarDB();
-        this.middlewares();
-        this.routes();
-    }
+  async conectarDB() {
+    await dbConnection();
+  }
 
-    async conectarDB(){
-        await dbConnection();
-    }
+  middlewares() {
+    this.app.use(express.static("public"));
+    this.app.use(cors());
+    this.app.use(express.json());
+  }
 
-    middlewares(){
-        this.app.use(express.static('public'));
-        this.app.use(cors());
-        this.app.use(express.json());
-    }
+  routes() {
+    this.app.use(this.usuariosPath, require("../routes/user.routes.js"));
+  }
 
-    routes(){
-        this.app.use(this.usuariosPath, require('../routes/user.routes'));
-    }
-
-    listen(){
-        this.app.listen(this.port, () =>{
-            console.log('Servidor ejecutansose y escuchando en el puerto', this.port)
-        });
-    }
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Servidor ejecutansose y escuchando en el puerto", this.port);
+    });
+  }
 }
 
 module.exports = Server;
